063f02d026707f248267c06da5f5f51722760d96
Simao Gomes Viana <xdevs23@outlook.com>
2017-02-04 17:29:26 +0100

    touchscreen: fine-tune optimizations
    Change-Id: I33b211555c6519d299ef019112eb77a84f9907a3
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    Signed-off-by: ShreyanshLodha <slodha96@gmail.com>

---
+++ drivers/input/touchscreen/touchopt.mk
@@ -0,0 +1,14 @@
+
+ccflags-y ?=
+ccflags-y += -O3 \
+    -fno-defer-pop \
+    -ffp-contract=fast \
+    -fgcse-sm \
+    -fgcse-las \
+    -fgcse-after-reload \
+    -fno-signed-zeros \
+    -fno-trapping-math \
+    -fno-associative-math \
+    -fno-reciprocal-math \
+    -ffinite-math-only \
+    -mtune=cortex-a57.cortex-a53 
