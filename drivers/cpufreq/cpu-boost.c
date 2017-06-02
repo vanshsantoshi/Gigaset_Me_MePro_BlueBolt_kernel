b67273b476ea19fd79c3e3753deba8f374b2638b
ShreyanshLodha <slodha96@gmail.com>
2017-06-02 09:33:06 +0530

    Added touchboost control

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -25,6 +25,16 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
+#include "../../kernel/sched/sched.h"
+
+#ifdef CONFIG_TOUCHBOOST_CONTROL
+#include <linux/export.h>
+unsigned int touchboost_status_1 = 0;
+unsigned int touchboost_freq_1 = 1344000;
+unsigned int touchboost_status_2 = 0;
+unsigned int touchboost_freq_2 = 1344000;
+unsigned int touchboost_ms = 0;
+#endif
 
 struct cpu_sync {
 	struct task_struct *thread;
